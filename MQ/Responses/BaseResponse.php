60de331250a404d18edb5eb7ae4401253cb6d6aa
LeiGuang <gooodfisher@126.com>
2026-04-02 17:12:36 +0800

    优化效率

--- MQ/Responses/BaseResponse.php
+++ MQ/Responses/BaseResponse.php
@@ -52,19 +52,13 @@ abstract class BaseResponse
 
     protected function loadAndValidateXmlContent($content, &$xmlReader)
     {
-        // ====================== 核心修复：清洗非法字符 ======================
-        // 过滤 XML 不允许的非法字符：0xFFFE / 0x00~0x1F 等乱码
-        $content = preg_replace('/[^\x09\x0A\x0D\x20-\x{D7FF}\x{E000}-\x{FFFD}]/u', '', $content);
-        libxml_use_internal_errors(true); // 屏蔽 XML 错误警告
-        
         $doc = new \DOMDocument();
         if(!$doc->loadXML($content)) {
-            libxml_clear_errors();
-            
-            return false;
+            $content = preg_replace('/[^\x{0009}\x{000A}\x{000D}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]/u', '', $content);
+            if(!$doc->loadXML($content)) {
+               return false;
+            }
         }
-        libxml_clear_errors();
-        
         $xmlReader = $this->loadXmlContent($content);
         return true;
     }
